f97f83678f49ae7055c1284d473d90923dc481b5
touati.nora <touati.nora@gmail.com>
2011-10-17 14:19:26 +0000

    The code is modified to take into account the new format of data

--- code/variatorHazmatJava/src/graph/Arc.java
+++ code/variatorHazmatJava/src/graph/Arc.java
@@ -10,20 +10,22 @@ public class Arc {
 	
 	private Integer num; 				// numero of the arc
 	private double cost; 			// cost of the arc
-	private double rCost; 			// reduced cost of the arc
-	private double totRisk; 		// The total risk on the arc (for all the regions)
+	//private double rCost; 			// reduced cost of the arc
+	//private double totRisk; 		// The total risk on the arc (for all the regions)
 	private Node origNode; 			// origin node of the arc
 	private Node destNode; 			// destination node of the arc
-	private Vector comRegRisk; 				// com --> Reg--> risk
+	//private Vector comRegRisk; 				// com --> Reg--> risk
+	
 	// TODO: why do we need a list of successor and predecessor nodes for each arc here? (Dimo)
 	private ArrayList<Node> succNodes;		// Successor Nodes of the arc
 	private ArrayList<Node> predNodes;		// predecessor Nodes of the arc
-	private ArrayList<ArrayList<Integer>>  mapRisk; // commodity - region - risk 
+	//private ArrayList<ArrayList<Integer>>  mapRisk; // commodity - region - risk 
+	private ArrayList<ArrayList<Double>>  comRisk; // commodity - risk 
 	
 	public Arc(){
 		this.succNodes = new ArrayList<Node>();
 		this.predNodes = new ArrayList<Node>();
-		this.mapRisk = new ArrayList<ArrayList<Integer>>();
+		this.comRisk = new ArrayList<ArrayList<Double>>();
 	}
 
 	public Arc(Node v1, Node v2){
@@ -31,7 +33,7 @@ public class Arc {
 	  this.destNode = v2;
 	  this.succNodes = new ArrayList<Node>();
 	  this.predNodes = new ArrayList<Node>();
-	  this.mapRisk = new ArrayList<ArrayList<Integer>>();
+	  this.comRisk = new ArrayList<ArrayList<Double>>();
 	}
 	
 	public Node returnOrigNode(){
@@ -54,13 +56,13 @@ public class Arc {
 	  return num;
 	}
 
-	public double returnReducedCost() {
-	  return rCost;
-	}
+	//public double returnReducedCost() {
+	// return rCost;
+	//}
 
-	public void setReducedCost(double cr) {
-		this.rCost = cr;
-	}
+	//public void setReducedCost(double cr) {
+	//this.rCost = cr;
+	//}
 
 	public double returnCost() {
 	  return cost;
@@ -70,30 +72,44 @@ public class Arc {
 	  this.cost = cou;
 	}
 	
-	public void setRisk(int c, int reg, int risk) {
-		ArrayList<Integer> v = new ArrayList<Integer>(3);
+	//public void setRisk(int c, int reg, int risk) {
+	//ArrayList<Integer> v = new ArrayList<Integer>(3);
+	//v.add(0, c);
+		//v.add(1, reg);
+	//v.add(2, risk);
+	//this.mapRisk.add(v);		
+	//}
+	
+	public void setRisk(Double c, Double risk) {
+		ArrayList<Double> v = new ArrayList<Double>(2);
 		v.add(0, c);
-		v.add(1, reg);
-		v.add(2, risk);
-		this.mapRisk.add(v);		
-	}
-	public int getRiskSize() {
-		return this.mapRisk.size();
+		//v.add(1, reg);
+		v.add(1, risk);
+		this.comRisk.add(v);		
 	}
 	
-	public ArrayList<ArrayList<Integer>> getMapRisk() {
-		return this.mapRisk;
+	public Double getRisk(int c) {
+		for (Integer i = 0; i < this.comRisk.size(); i++) {
+			if (this.comRisk.get(i).get(0) == c ) {
+				return this.comRisk.get(i).get(1);
+			}
+		}	
+		return -1.0;
 	}
+	
+	//public ArrayList<ArrayList<Integer>> getMapRisk() {
+	//return this.mapRisk;
+	//}
 	
 	/* returns risk on region reg when this arc is used to transport commodity c */
-	public double getRisk(int c, int reg) {
+	//public double getRisk(int c, int reg) {
 		// parcourir mapRisk
-		for (int i = 0; i < this.mapRisk.size(); i++) {
-			if (this.mapRisk.get(i).get(0) == c && this.mapRisk.get(i).get(1) == reg) {
-					return this.mapRisk.get(i).get(2);
-			}
-		}		
-		System.out.println("Error, risk not found");	
-		return -1;
-	}
+	//for (int i = 0; i < this.mapRisk.size(); i++) {
+	//if (this.mapRisk.get(i).get(0) == c && this.mapRisk.get(i).get(1) == reg) {
+	//	return this.mapRisk.get(i).get(2);
+	//}
+	//}		
+	//System.out.println("Error, risk not found");	
+	//return -1;
+	//}
 }
--- code/variatorHazmatJava/src/mainTest.java
+++ code/variatorHazmatJava/src/mainTest.java
@@ -10,17 +10,19 @@ public class mainTest {
 		   System.out.println("1. Lecture du fichier de donnee");		   
 		   Graph G = new Graph();
 		   G.readInstance(args[0]);
+		   System.out.println("Fin de lecture de l'instance");	
 		   Commodity c = G.returnListCommodities().get(0);
 		   int indexO = c.getSource();
 		   Node o = G.returnNode(indexO);
 		   int indexD = c.getDest();
 		   Node d = G.returnNode(indexD);
+		   System.out.println("Commodite 0: Origine = "+o.get_numero()+", Dest = "+d.get_numero());	
 		   ArrayList<Node> sPath= G.shortestPath(o,d);
-		   
-		   // Test		   
-		   Arc arc = G.getArc(3, 6);		      
-		   double risk = arc.getRisk(1, 1);
-		   System.out.println("3  6  1  1 = " + risk);	
+		   // Test	
+		   System.out.println("Je suis laaaaaaa11111111111");	
+		   Arc arc = G.getArc(39, 38);		      
+		   double risk = arc.getRisk(0);
+		   System.out.println("r(39, 38) = " + risk);	
 		   
 		   G.graphViz(args[0]);
 	   }
