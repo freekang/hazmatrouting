fe975dc75fc771652d20968ce18b755629bd9019
brockho <brockho@lix.polytechnique.fr>
2011-11-10 15:09:15 +0000

    updated plot of population after 1000 generations of HypE with both initialization schemes

--- DAW_hazmatHypE/trunk/experiments/randVsCost/plot3d_after1000gen.m
+++ DAW_hazmatHypE/trunk/experiments/randVsCost/plot3d_after1000gen.m
@@ -1,39 +1,25 @@
 % plot resulting populations after 1000 generations
 
 C = dlmread('results/graph_ns2_1_New_costInit.1000');
+C_nondom = C(find(paretoFront(C(:,1:3))),:);
+%plot3(C(:,1),C(:,2),C(:,3), 'x');
+%hold all; grid on;
+%plot3(C_nondom(:,1),C_nondom(:,2),C_nondom(:,3), 'o');
+
+
 R = dlmread('results/graph_ns2_1_New_randInit.1000');
-
-subplot(2,2,1)
-plot3(C(:,1),C(:,2),C(:,3), 'x');
-hold all; grid on;
-plot3(R(:,1),R(:,2),R(:,3), 'o');
-xlabel('cost')
-ylabel('total risk')
-zlabel('equity risk')
+R_nondom = R(find(paretoFront(R(:,1:3))),:);
+%figure()
+%plot3(R(:,1),R(:,2),R(:,3), 'x');
+%hold all; grid on;
+%plot3(R_nondom(:,1),R_nondom(:,2),R_nondom(:,3), 'o');
 
 
-subplot(2,2,2)
-plot(C(:,1),C(:,2), 'x');
-hold all; grid on;
-plot(R(:,1),R(:,2), 'o');
-legend('shortest path initialization', 'random initialization');
-xlabel('cost')
-ylabel('total risk')
 
+%mysubplot(C,C_nondom, 'C', 'C_nondom', 'C only', 'filename');
+%mysubplot(R,R_nondom, 'R', 'R_nondom', 'R only', 'filename');
 
-subplot(2,2,3)
-plot(C(:,1),C(:,3), 'x');
-hold all; grid on;
-plot(R(:,1),R(:,3), 'o');
-legend('shortest path initialization', 'random initialization');
-xlabel('cost')
-ylabel('equity risk')
+mysubplot(C,R, 'shortest path initialization', 'random initialization', 'all solutions', 'allsolutions.eps')
+mysubplot(C_nondom,R_nondom, 'shortest path initialization', 'random initialization', 'nondominated', 'onlynondominated.eps')
 
-
-subplot(2,2,4)
-plot(C(:,2),C(:,3), 'x');
-hold all; grid on;
-plot(R(:,2),R(:,3), 'o');
-legend('shortest path initialization', 'random initialization');
-xlabel('total risk')
-ylabel('equity risk')
+hold off;
