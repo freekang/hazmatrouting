a44fd3cdd63d4e866e1decaf5577db615fe7709f
brockho@lix.polytechnique.fr <brockho@lix.polytechnique.fr@8e9a91b5-ae2d-7083-5045-808bc6eadb1e>
2011-11-28 14:15:49 +0000

    added box plots comparing HypE with popsize 50 and 100 over time

---
+++ trunk/DAW_hazmatHypE/trunk/experiments/randVsCost/hypervolumes/hypervolumeAlgoComparisons.m
@@ -0,0 +1,30 @@
+addpath('../');
+
+hypervolumes = zeros(10, 5);
+
+% HypE popsize 25
+filename = 'HYP_ns4_1_OriginalCosts_HypE_popsize25_randInit.4000';
+hypervolumes(:,1) = -1 * dlmread(filename);
+% HypE popsize 50
+filename = 'HYP_ns4_1_OriginalCosts_HypE_popsize50_randInit.2000';
+hypervolumes(:,2) = -1 * dlmread(filename);
+% HypE popsize 100
+filename = 'HYP_ns4_1_OriginalCosts_randInit.1000';
+hypervolumes(:,3) = -1 * dlmread(filename);
+% HypE popsize 200
+filename = 'HYP_ns4_1_OriginalCosts_HypE_popsize200_randInit.500';
+hypervolumes(:,4) = -1 * dlmread(filename);
+% NSGA-II
+filename = 'HYP_ns4_1_OriginalCosts_nsga2_randInit.1000';
+hypervolumes(:,5) = -1 * dlmread(filename);
+
+
+
+
+boxplot(hypervolumes, 'Labels', {'HypE_25'; 'HypE_50'; 'HypE_100'; 'HypE_200'; 'NSGA-II'});
+
+
+
+% save plot as eps:
+exportfig(gcf, 'algoComparison.eps');
+
